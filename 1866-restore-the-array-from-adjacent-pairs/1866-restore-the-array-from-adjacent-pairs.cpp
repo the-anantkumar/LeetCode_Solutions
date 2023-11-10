75274e81d9b274656a26a80281bc223435848f8f
Anant Kumar Srivastava <66388222+the-anantkumar@users.noreply.github.com>
2023-11-10 23:20:14 +0530

    Time: 965 ms (5.15%), Space: 174.5 MB (5.15%) - LeetHub

---
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    map<int, vector<int>>adj;
+    vector<int> answer;
+    map<int, bool> vis;
+
+    void dfs(int src)
+    {
+        answer.push_back(src);
+        vis[src] = 1;
+
+        for(auto x: adj[src])
+        {
+            if(!vis[x])
+            {
+                dfs(x);
+            }
+        }
+    }
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        int n = adjacentPairs.size();
+
+       // adj.resize();
+     
+        for(auto x: adjacentPairs)
+        {
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+            vis[x[0]] = false;
+            vis[x[1]] = false;
+        }
+        int root = INT_MIN;
+        for(auto x: adj)
+        {
+            if(x.second.size() == 1) root = x.first;
+        }
+
+        dfs(root);
+
+        return answer;
+    }
+};
